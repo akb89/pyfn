8ea7730945daddd84dcbb677c64c47f7461965c0
akb89 <akb@3azouz.net>
2017-09-07 14:20:08 +0200

    Refactored unit tests to match new exemplar/lexunit wording

--- tests/test_unmarshallers_exemplar.py
+++ tests/test_unmarshallers_exemplar.py
@@ -2,21 +2,21 @@
 
 import os
 
-import pyFN.marshalling.unmarshallers.lexunit as lexunit_unmarshaller
+import pyFN.marshalling.unmarshallers.exemplar as exemplar_unmarshaller
 
 
 LU_XML_FILE = os.path.join(os.path.dirname(__file__), 'resources', 'lu.xml')
 
-lu_annosets_list = list(lexunit_unmarshaller.unmarshall_lexunit_xml(LU_XML_FILE))
+ex_annosets_list = list(exemplar_unmarshaller.unmarshall_exemplar_xml(LU_XML_FILE))
 
 
 def test_annosets_counts():
-    assert len(lu_annosets_list) == 2
-    assert len([item for sublist in lu_annosets_list for item in sublist]) == 2
+    assert len(ex_annosets_list) == 2
+    assert len([item for sublist in ex_annosets_list for item in sublist]) == 2
 
 
 def test_lu_annoset():
-    annoset = lu_annosets_list[1][0]
+    annoset = ex_annosets_list[1][0]
     assert annoset._id == 310929
     assert len(annoset.vustore.valence_units) == 6
     assert annoset.valence_pattern.with_fe_name ==\
@@ -24,7 +24,7 @@ def test_lu_annoset():
 
 
 def test_lu_target():
-    annoset = lu_annosets_list[0][0]
+    annoset = ex_annosets_list[0][0]
     assert annoset.target.string == 'Arch'
     assert annoset.target.lexunit._id == 65
     assert annoset.target.lexunit.name == 'arch.v'
