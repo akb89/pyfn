153d4bfcdf58a704a25651372c98a60b731efc39
akb89 <akb@3azouz.net>
2018-02-23 19:22:48 +0100

    Added kbest and splits parameters

--- scripts/rofames.sh
+++ scripts/rofames.sh
@@ -4,14 +4,15 @@ source "$(dirname "${BASH_SOURCE[0]}")/setup.sh"
 
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-h] -m {train,decode} -x XPDIR -u -t {mxpost,nlp4j}
+Usage: ${0##*/} [-h] -m {train,decode} -x XP_DIR -s {dev,test} -t {mxpost,nlp4j} [-u]
 Train or decode with the ROFAMES parser.
 
-  -h, --help                          display this help and exit
-  -m, --mode          {train,decode}  rofames mode to use: train or decode
-  -x, --xpdir         XPDIR           absolute path to the xp directory (where data/ and model/ will be stored)
-  -u, --use_hierarchy                 if specified, parser will use the hierarchy feature
-  -t, --tagger        {mxpost,nlp4j}  the POS tagger used for preprocessing splits
+  -h, --help                           display this help and exit
+  -m, --mode          {train,decode}   rofames mode to use: train or decode
+  -x, --xpdir         XP_DIR           absolute path to the xp directory (where data/ and model/ will be stored)
+  -s, --splits        {dev,test}       which splits to use: dev or test
+  -t, --tagger        {mxpost,nlp4j}   the POS tagger used for preprocessing splits
+  -u, --use_hierarchy                  if specified, parser will use the hierarchy feature
 EOF
 }
 
@@ -19,6 +20,7 @@ is_mode_set=FALSE
 is_xpdir_set=FALSE
 use_hierarchy=FALSE
 is_tagger_set=FALSE
+is_splits_set=FALSE
 
 while :; do
     case $1 in
@@ -44,19 +46,28 @@ while :; do
                 die "ERROR: '--xpdir' requires a non-empty option argument"
             fi
             ;;
-      -u|--use_hierarchy)
-            use_hierarchy=TRUE
-            shift
+        -s|--splits)
+            if [ "$2" ]; then
+                is_splits_set=TRUE
+                splits=$2
+                shift
+            else
+                die "ERROR: '--splits' requires a non-empty option argument"
+            fi
             ;;
-      -t|--tagger)
-          if [ "$2" ]; then
-              is_tagger_set=TRUE
-              tagger=$2
+        -t|--tagger)
+            if [ "$2" ]; then
+                is_tagger_set=TRUE
+                tagger=$2
+                shift
+            else
+                die "ERROR: '--tagger' requires a non-empty option argument"
+            fi
+            ;;
+        -u|--use_hierarchy)
+              use_hierarchy=TRUE
               shift
-          else
-              die "ERROR: '--tagger' requires a non-empty option argument"
-          fi
-          ;;
+              ;;
         --)
             shift
             break
@@ -82,6 +93,10 @@ if [ "${is_tagger_set}" = FALSE ]; then
     die "ERROR: '--tagger' parameter is required."
 fi
 
+if [ "${is_splits_set}" = FALSE ]; then
+    die "ERROR: '--splits' parameter is required."
+fi
+
 case "${mode}" in
     train )
         ;;
@@ -97,7 +112,16 @@ case "${tagger}" in
     nlp4j )
         ;;
     * )
-        die "Invalid POS tagger '${tagger}': Should be 'mxpost' or 'nlp4j'"
+        die "Invalid POS tagger '${tagger}': should be 'mxpost' or 'nlp4j'"
+esac
+
+case "${splits}" in
+    dev )
+        ;;
+    test )
+        ;;
+    * )
+        die "Invalid splits '${splits}': should be 'dev' or 'test'"
 esac
 
 if [ "${mode}" = train ]; then
@@ -106,6 +130,7 @@ if [ "${mode}" = train ]; then
     ${XPDIR} \
     ${tagger}
     ${lambda} \
+    ${kbest} \
     ${batch_size} \
     ${save_every_k_batches} \
     ${num_models_to_save} \
@@ -120,7 +145,9 @@ if [ "${mode}" = decode ]; then
   bash ${ROFAMES_HOME}/bin/decode.sh \
     ${JAVA_HOME_BIN} \
     ${XPDIR} \
+    ${splits} \
     ${tagger} \
+    ${kbest} \
     ${max_ram} \
     ${use_hierarchy} \
     ${LOGS_DIR}
