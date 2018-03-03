ff5ca9a044c957d94eddf3a871b6e861c9c82805
akb89 <akb@3azouz.net>
2018-03-03 17:45:04 +0100

    Updated scoring script to include which parser to use: rofames or open-sesame

--- scripts/score.sh
+++ scripts/score.sh
@@ -4,16 +4,18 @@ source "$(dirname "${BASH_SOURCE[0]}")/setup.sh"
 
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-h] -x XP_NUM -s {dev,test}
+Usage: ${0##*/} [-h] -x XP_NUM -p {rofames,open-sesame} -s {dev,test}
 Score frame semantic parsing with the SEMEVAL scoring scripts modified by Kshirsagar et al. (2015).
 
   -h, --help                display this help and exit
   -x, --xp      XP_NUM      xp number written as 3 digits (e.g. 001)
+  -p, --parser  {rofames,open-sesame}  frame semantic parser to be used: 'rofames' or 'open-sesame'
   -s, --splits  {dev,test}  which splits to score: dev or test
 EOF
 }
 
 is_xp_set=FALSE
+is_parser_set=FALSE
 is_splits_set=FALSE
 
 while :; do
@@ -29,6 +31,15 @@ while :; do
                 shift
             else
                 die "ERROR: '--xp' requires a non-empty option argument"
+            fi
+            ;;
+        -p|--parser)
+            if [ "$2" ]; then
+                is_parser_set=TRUE
+                parser=$2
+                shift
+            else
+                die "ERROR: '--parser' requires a non-empty option argument"
             fi
             ;;
         -s|--splits)
@@ -57,9 +68,22 @@ if [ "${is_xp_set}" = FALSE ]; then
     die "ERROR: '--xp' parameter is required."
 fi
 
+if [ "${is_parser_set}" = FALSE ]; then
+    die "ERROR: '--parser' parameter is required."
+fi
+
 if [ "${is_splits_set}" = FALSE ]; then
     die "ERROR: '--splits' parameter is required."
 fi
+
+case "${parser}" in
+    rofames )
+        ;;   #fallthru
+    open-sesame )
+        ;;   #fallthru
+    * )
+        die "Invalid frame semantic parser '${parser}': Should be 'rofames' or 'open-sesame'"
+esac
 
 case "${splits}" in
     dev )
@@ -72,14 +96,27 @@ esac
 
 mkdir ${XP_DIR}/${xp}/score 2> /dev/null
 
-echo "Converting decoded file to SEMEVAL XML format..."
-pyfn convert \
-  --from bios \
-  --to semeval \
-  --source "${XP_DIR}/${xp}/data/${splits}.bios.semeval.merged.decoded" \
-  --target "${XP_DIR}/${xp}/data/${splits}.predicted.xml" \
-  --sent "${XP_DIR}/${xp}/data/${splits}.sentences"
-echo "Done"
+if [ "${parser}" = "rofames" ]; then
+  echo "Converting decoded .frame.elements file to SEMEVAL XML format..."
+  pyfn convert \
+    --from rofames \
+    --to semeval \
+    --source "${XP_DIR}/${xp}/data/${splits}.frame.elements" \
+    --target "${XP_DIR}/${xp}/data/${splits}.predicted.xml" \
+    --sent "${XP_DIR}/${xp}/data/${splits}.sentences"
+  echo "Done"
+fi
+
+if [ "${parser}" = "open-sesame" ]; then
+  echo "Converting decoded .bios file to SEMEVAL XML format..."
+  pyfn convert \
+    --from bios \
+    --to semeval \
+    --source "${XP_DIR}/${xp}/data/${splits}.bios.semeval.merged.decoded" \
+    --target "${XP_DIR}/${xp}/data/${splits}.predicted.xml" \
+    --sent "${XP_DIR}/${xp}/data/${splits}.sentences"
+  echo "Done"
+fi
 
 echo "Scoring with Kshirsagar et al. (2015) ACL perl script..."
 perl ${SEMEVAL_HOME}/score.acl.pl \
